df626e660ff1327d4bed35b322ffc88eaf2bdc26
Justin-Storm <144840724+Justin-Storm@users.noreply.github.com>
2025-01-27 13:16:36 -0500

    Delete Random-Testimonial directory

--- Random-Testimonial/script/script.js
+++
@@ -1,4 +0,0 @@
-string = document.getElementById('press-quote');
-myArray = ['NAPS is by far the most significant cultural force of the decade. — New York Times', 'Text 2', 'Text 1'];
-number = Math.floor(Math.random() * 3);
-string.innerHTML = myArray[number];
